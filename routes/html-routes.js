48f5e03e660d436a24b11706c417ce63daeab177
shakib <shakib1122@gmail.com>
2020-08-17 17:33:19 +1000

    handlebar initial upload

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,20 +10,25 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/members");
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
+    // res.sendFile(path.join(__dirname, "../public/signup.html"));
+    res.render("signup");
   });
 
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
+      console.log("has account");
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+    // res.sendFile(path.join(__dirname, "../public/login.html"));
+    res.render("login");
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+  app.get("/members", (req, res) => {
+    // res.sendFile(path.join(__dirname, "../public/members.html"));
+    // ***this is the original route that brings you to the members webpage
+    res.render("members");
   });
 };
